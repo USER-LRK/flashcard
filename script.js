8bb19b5c506854dee756806fa21fa37b051db4c3
USER-LRK <renkaitbc@gmail.com>
2025-04-22 14:26:01 +0800

    update on def

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let filteredFlashcards = [];
 
 // Load flashcards from JSON
 async function loadFlashcards() {
-    const response = await fetch('final_output.json');
+    const response = await fetch('final_outputv2.json');
     flashcards = await response.json();
     populateLevelDropdown();
 }
